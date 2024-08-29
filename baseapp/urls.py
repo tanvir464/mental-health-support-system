02537ceaee31b681d5575f11d32033c816109ae4
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-08-29 17:28:24 +0600

    Delete baseapp directory

--- baseapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "baseapp"
-urlpatterns = [
-    path('', views.landpage, name="landpage"),
-    path('admin_dashboard/', views.admin_dashboard, name="admin_dashboard"),    
-]
--- baseapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def landpage(request) :
-    return render(request, "baseapp/landpage.html")
-
-def admin_dashboard(request) :
-    return render(request, "baseapp/admin_dashboard.html")
